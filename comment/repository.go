39d7dec0a8988ec237923bfc3dbd709b0f6e811d
Sahibul Nuzul Firdaus <sahibulnuzulfirdaus13@gmail.com>
2021-06-14 11:58:14 +0700

    👍 DONE: Respository for Get Comment By ForumID and TaskID

--- comment/repository.go
+++ comment/repository.go
@@ -1,9 +1,14 @@
 package comment
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Repository interface {
 	Save(comment Comment) (Comment, error)
+	// FindALl() ([]Comment, error)
+	FindByTaskID(taskID int) ([]Comment, error)
+	FindByForumID(forumID int) ([]Comment, error)
 }
 
 type repository struct {
@@ -22,4 +27,40 @@ func (r *repository) Save(comment Comment) (Comment, error) {
 	}
 
 	return comment, nil
+}
+
+// func (r *repository) FindALl() ([]Comment, error) {
+// 	var comments []Comment
+
+// 	err := r.db.Find(&comments).Error
+
+// 	if err != nil {
+// 		return comments, err
+// 	}
+
+// 	return comments, nil
+// }
+
+func (r *repository) FindByForumID(forumID int) ([]Comment, error) {
+	var comments []Comment
+
+	err := r.db.Where("forum_id = ?", forumID).Find(&comments).Error
+
+	if err != nil {
+		return comments, err
+	}
+
+	return comments, nil
+}
+
+func (r *repository) FindByTaskID(taskID int) ([]Comment, error) {
+	var comments []Comment
+
+	err := r.db.Where("task_id = ?", taskID).Find(&comments).Error
+
+	if err != nil {
+		return comments, err
+	}
+
+	return comments, nil
 }
