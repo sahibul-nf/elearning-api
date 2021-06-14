7bfc8ee2d6859b14654695fb4394b28cfd3a325f
Sahibul Nuzul Firdaus <sahibulnuzulfirdaus13@gmail.com>
2021-06-14 12:02:34 +0700

    ✨ NEW FEATURE: Authentication with JWT

---
+++ auth/service.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"elearning-api/consts"
+	"errors"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Service interface {
+	GenerateToken(userID int) (string, error)
+	ValidateToken(token string) (*jwt.Token, error)
+}
+
+type jwtService struct {
+}
+
+func NewService() *jwtService {
+	return &jwtService{}
+}
+
+func (s *jwtService) GenerateToken(userID int) (string, error) {
+	claim := jwt.MapClaims{}
+	claim["user_id"] = userID
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	signedToken, err := token.SignedString(consts.SecretKey())
+
+	if err != nil {
+		return signedToken, err
+	}
+
+	return signedToken, nil
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+
+	token, err := jwt.Parse(encodedToken, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+
+		return []byte(consts.SecretKey()), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
+
+}
